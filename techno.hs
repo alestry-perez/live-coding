e723380a247d03ea8b5f0e6aa60357ee90ef22a4
Alestry Pérez <alestry.perez@hyperisland.se>
2021-02-03 00:34:21 +0100

    Create techno.hs

---
+++ techno.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Sound.Tidal.Drum.Techno where
+
+import Sound.Tidal.Context
+
+bps = 1
+
+bd = "[t ~ ~ ~] [t ~ ~ ~] [t ~ ~ ~] [t ~ ~ ~] " :: Pattern Bool
+sn = "[~ ~ ~ ~] [t ~ ~ ~] [~ ~ ~ ~] [t ~ ~ ~] " :: Pattern Bool
+ch = "[~ ~ ~ ~] [~ ~ ~ ~] [~ t ~ ~] [~ ~ ~ ~] " :: Pattern Bool
+oh = "[~ ~ t ~] [~ ~ t ~] [~ ~ t ~] [~ ~ t ~] " :: Pattern Bool
